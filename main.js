70c85db082a0ad0e300c72a4a0f8960882898266
Jay-tie <daoduytien131200@gmail.com>
2023-01-06 10:51:42 +0700

    6th/1 update

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-var btn1=1;
-var btn1=2;
-var btn1=3;
-var btn1=4;
-var btn1=5;
-var btn1=6;
-var btn1=7;
-var btn1=8;
-var btn1=9;
+             function AddNum(num){
+            document.getElementById('display').value+=num
+        }
+         // đặt tên function là AddNum 
+        //  (num) là yếu tố được gọi ra khi function được thực hiện . 
+        // vậy khi
+        
+        function AddOp(op){
+            document.getElementById('display').value+=op
+        }
+        // += là 1 operator thực hiện phép cộng cho cả dạng data number và string
+        
+        function Cal(){
+            var display = document.getElementById('display').value;
+            var result = eval(display)
+            document.getElementById('result').innerHTML = result
+            
+        }
