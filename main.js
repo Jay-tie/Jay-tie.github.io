104c7e6abe34af66f6e44addc4734627a2a461a0
Jay-tie <daoduytien131200@gmail.com>
2023-01-08 10:47:47 +0700

    8th/1 update

--- main.js
+++ main.js
@@ -4,12 +4,11 @@
          // đặt tên function là AddNum 
         //  (num) là yếu tố được gọi ra khi function được thực hiện . 
         // vậy khi
-        
+        // += là 1 operator thực hiện phép cộng cho cả dạng data number và string
         function AddOp(op){
             document.getElementById('display').value+=op
+           
         }
-        // += là 1 operator thực hiện phép cộng cho cả dạng data number và string
-        
         function Cal(){
             var display = document.getElementById('display').value;
             var result = eval(display)
