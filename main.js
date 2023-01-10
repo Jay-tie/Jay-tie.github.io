bdd555cc251405842b8d6293506ff770b6374aa2
Jay-tie <121649938+Jay-tie@users.noreply.github.com>
2023-01-10 17:07:10 +0700

    Add files via upload
    completed project at 5:06pm on 10th january

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
          function AddNum(num){
          document.getElementById('display').value+=num
         }
@@ -6,22 +7,22 @@
         // vậy khi
         
         function AddOp(op){
-            document.getElementById('display').value+=op
-            var display = document.getElementById('display').value;
-            // var LastChar = display.value[display.value.length - 1, display.value.length - 1];
-        var LastChar=display.value.substring(display.value.length - 1, display.value.length - 1);
-            if (LastChar==='+'||LastChar==='-'||LastChar==='*'||LastChar==='/'){
-                return;
+            // document.getElementById('display').value+=op
+            var lastchar = display.value[display.value.length - 1];
+            // var lastchar = display.value.substring(display.value.length - 1, display.value.length - 1);
+            if (lastchar==='+'||lastchar=='-'||lastchar==='*'||lastchar==='/'){
+                display.value = display.value.substring(0, display.value.length - 1);
+
             }
-           else {display.value+=op;
+            display.value+=op
 
         }
-        }
+        
         // += là 1 operator thực hiện phép cộng cho cả dạng data number và string
         
         function Cal(){
             var display = document.getElementById('display').value;
-            var result = eval(display)
+            var result = eval(display);
             document.getElementById('result').innerHTML = result
             
         }
@@ -37,8 +38,8 @@
         // (..., display.value.lenght -1 ) là vị trí kết thúc của chuỗi mới trong trường hợp trên là độ dài của chuỗi cũ -1
 
         function AC(){
-            document.getElementById('display').value = '';
-            document.getElementById('result').innerHTML = '';
+            document.getElementById('display').value = ''
+            document.getElementById('result').innerHTML = ''
         }
         // set 2 biến về rỗng
             // var display = document.getElementById('display').value;
